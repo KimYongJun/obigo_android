a4c22956eab5ebd649034f38186bf4589444c4b6
KimYongJun <ddbd511@gmail.com>
2016-12-20 16:20:55 +0900

    Firebase Push Message Content 추가

--- app/src/main/java/com/obigo/obigoproject/service/FirebaseMessagingService.java
+++ app/src/main/java/com/obigo/obigoproject/service/FirebaseMessagingService.java
@@ -13,6 +13,12 @@ import com.obigo.obigoproject.R;
 import com.obigo.obigoproject.activity.MessageActivity;
 import com.obigo.obigoproject.util.WakeUpScreenUtil;
 
+/**
+ * Created by O BI HE ROCK on 2016-12-14
+ * 김용준, 최현욱
+ * FirebaseMessagingService 서버에서 발송한 메시지를 받음
+ * 전달받은 메시지를 클릭하면 MessageActivity로 이동
+ */
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService{
     private static final String TAG = "FirebaseMsgService";
@@ -20,15 +26,18 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     // 서버에서 Push Message 전달시 받는 작업
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        sendNotification(remoteMessage.getData().get("message"), remoteMessage.getData().get("title"));
+        String title = remoteMessage.getData().get("title");
+        String content = remoteMessage.getData().get("content");
+
+        sendNotification(title, content);
     }
 
     // 메시지를 받음
-    private void sendNotification(String content, String title) {
+    private void sendNotification(String title, String content) {
         // 화면이 켜지는 작업 - 10초 유지
         WakeUpScreenUtil.acquire(getApplicationContext(), 10000);
 
-        // 메시지를 클릭하면 메[시지 리스트로 이동
+        // 메시지를 클릭하면 메시지 리스트로 이동
         Intent intent = new Intent(this, MessageActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
